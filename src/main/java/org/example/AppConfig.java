4eae06dbd00dbb1e34b73ed188b852d3c59fad37
tomyjany <tomas.janovec@tul.cz>
2024-03-06 13:47:37 +0100

    add spring boot and libs

---
+++ src/main/java/org/example/AppConfig.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cz.tul.sti2024.cv")
+public class AppConfig {}
---
+++ src/main/java/org/example/StinApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StinApplication.class, args);
+    }
+
+}
